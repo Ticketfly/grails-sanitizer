a5987982b247ab291f2f54216df7f2557363e94b
SteveConnelly <steve.connelly@objectpartners.com>
2014-10-09 18:18:37 -0700

    Compatibility fixes for Grails 2.4.x

--- grails-app/utils/org/grails/plugins/sanitizer/SanitizedMarkupCodec.groovy
+++ grails-app/utils/org/grails/plugins/sanitizer/SanitizedMarkupCodec.groovy
@@ -1,8 +1,4 @@
 package org.grails.plugins.sanitizer
-
-import org.codehaus.groovy.grails.commons.ApplicationHolder
-import org.codehaus.groovy.grails.commons.ConfigurationHolder
-
 /**
  * Codec that allows you to sanitize a String
  * <br />def clean = myText.encodeAsSanitizedMarkup()
@@ -13,6 +9,7 @@ import org.codehaus.groovy.grails.commons.ConfigurationHolder
 class SanitizedMarkupCodec {
 
 	def markupSanitizerService
+    def grailsApplication
 
 	/**
 	 * Encode a string as sanitized Markup
@@ -20,12 +17,12 @@ class SanitizedMarkupCodec {
 	def encode = { dirtyMarkup ->
 
 		if(!markupSanitizerService){
-			markupSanitizerService = ApplicationHolder.application.mainContext.getBean("markupSanitizerService")
+			markupSanitizerService = grailsApplication.mainContext.getBean("markupSanitizerService")
 		}
 
 		MarkupSanitizerResult result = markupSanitizerService.sanitize(dirtyMarkup)
 
-		if(result.isInvalidMarkup() && !ConfigurationHolder.config?.sanitizer?.trustSanitizer){
+		if(result.isInvalidMarkup() && !grailsApplication.config?.sanitizer?.trustSanitizer){
 			// just return empty string...
 			return ""
 		}else{
